16bc7bb6fdbffa22cfeb9a02f8dbd84e7ce70d69
Merlin Baer <merlinbaer-metal@bruu.eu>
2026-01-03 19:45:02 +0100

    Fix in timining of setSession

--- hooks/useSupabase.ts
+++ hooks/useSupabase.ts
@@ -1,33 +1,42 @@
 import { useContext, useEffect, useState } from "react";
-
 import { SupabaseClient, Session } from "@supabase/supabase-js";
-
 import { SupabaseContext } from "@/components/authProvider";
 
 interface UseSupabaseProps {
   isLoaded: boolean;
-  session: Session | null | undefined;
+  session: Session | null;
   supabase: SupabaseClient;
 }
 
 export const useSupabase = (): UseSupabaseProps => {
   const supabase = useContext(SupabaseContext);
+
   const [session, setSession] = useState<Session | null>(null);
   const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     if (!supabase) return;
+
+    let mounted = true;
+
+    // Initial session with refresh-tryouts
     supabase.auth.getSession().then(({ data }) => {
-      setSession(data.session);
+      if (!mounted) return;
+      setSession(data.session ?? null);
       setIsLoaded(true);
     });
-    const { data: listener } = supabase.auth.onAuthStateChange(
-      (_event, newSession) => {
-        setSession(newSession);
-      },
-    );
+
+    // Auth Updates
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, newSession) => {
+      if (!mounted) return;
+      setSession(newSession ?? null);
+    });
+
     return () => {
-      listener.subscription.unsubscribe();
+      mounted = false;
+      subscription.unsubscribe();
     };
   }, [supabase]);
 
